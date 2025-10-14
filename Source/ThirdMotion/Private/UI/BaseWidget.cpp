cd00d0daeebae4dbf6f005660d3790d19723e69a
eunjung <jjonjunggame@gmail.com>
2025-10-14 17:41:18 +0900

    Fix::MainUIManager 삭제 -> 한 페이지이기 때문에 UserWidget에서 처리하도록 변경

--- Source/ThirdMotion/Private/UI/BaseWidget.cpp
+++ Source/ThirdMotion/Private/UI/BaseWidget.cpp
@@ -1,39 +1,15 @@
-// TwinmotionBaseWidget.cpp
-// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "UI/BaseWidget.h"
-#include "UI/UIManager.h"
 #include "Kismet/GameplayStatics.h"
 
 void UBaseWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 
-	// Get UI Manager reference
-	UIManager = GetUIManager();
-
-	if (UIManager)
-	{
-		OnTwinmotionUIInitialize();
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("TwinmotionBaseWidget: Failed to get UIManager reference"));
-	}
 }
 
 void UBaseWidget::NativeDestruct()
 {
-	OnTwinmotionUICleanup();
+	OnUICleanup();
 	Super::NativeDestruct();
-}
-
-UUIManager* UBaseWidget::GetUIManager() const
-{
-	UGameInstance* GameInstance = GetGameInstance();
-	if (GameInstance)
-	{
-		return GameInstance->GetSubsystem<UUIManager>();
-	}
-	return nullptr;
 }
--- Source/ThirdMotion/Private/UI/MainWidget.cpp
+++ Source/ThirdMotion/Private/UI/MainWidget.cpp
@@ -1,70 +1,35 @@
-// TwinmotionMainWidget.cpp
-// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "UI/MainWidget.h"
-#include "UI/UIManager.h"
 #include "Components/Widget.h"
 
 void UMainWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 
-	if (UIManager)
-	{
-		// Subscribe to panel visibility changes
-		UIManager->OnPanelVisibilityChanged.AddDynamic(this, &UMainWidget::OnPanelVisibilityChanged);
-	}
 }
 
-void UMainWidget::ToggleLeftPanel()
-{
-	if (UIManager)
-	{
-		bool bCurrentVisible = UIManager->IsPanelVisible(ETwinmotionPanelType::Library);
-		UIManager->SetPanelVisibility(ETwinmotionPanelType::Library, !bCurrentVisible);
-	}
-}
-
-void UMainWidget::ToggleRightPanel()
-{
-	if (UIManager)
-	{
-		bool bCurrentVisible = UIManager->IsPanelVisible(ETwinmotionPanelType::Scene);
-		UIManager->SetPanelVisibility(ETwinmotionPanelType::Scene, !bCurrentVisible);
-	}
-}
-
-void UMainWidget::ToggleBottomPanel()
-{
-	if (UIManager)
-	{
-		bool bCurrentVisible = UIManager->IsPanelVisible(ETwinmotionPanelType::Thumbnail);
-		UIManager->SetPanelVisibility(ETwinmotionPanelType::Thumbnail, !bCurrentVisible);
-	}
-}
-
-void UMainWidget::OnPanelVisibilityChanged(ETwinmotionPanelType PanelType, bool bVisible)
+void UMainWidget::OnPanelVisibilityChanged(EPanelType PanelType, bool bVisible)
 {
 	ESlateVisibility NewVisibility = bVisible ? ESlateVisibility::Visible : ESlateVisibility::Collapsed;
 
 	switch (PanelType)
 	{
-		case ETwinmotionPanelType::Library:
+		case EPanelType::Library:
 			if (LeftPanel)
 			{
 				LeftPanel->SetVisibility(NewVisibility);
 			}
 			break;
 
-		case ETwinmotionPanelType::Scene:
-		case ETwinmotionPanelType::Properties:
+		case EPanelType::Scene:
+		case EPanelType::Properties:
 			if (RightPanel)
 			{
 				RightPanel->SetVisibility(NewVisibility);
 			}
 			break;
 
-		case ETwinmotionPanelType::Thumbnail:
+		case EPanelType::Thumbnail:
 			if (BottomPanel)
 			{
 				BottomPanel->SetVisibility(NewVisibility);
