c685f873b9b9b613b4fab2cf5402084836b28939
Lauren <57754902+HyeseonLikesPizza@users.noreply.github.com>
2025-10-14 21:34:34 +0900

    [Temp] LibraryPanel 위젯 생성

---
+++ Source/ThirdMotion/Public/UI/LibraryPanel.h
@@ -0,0 +1,23 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseWidget.h"
+#include "LibraryPanel.generated.h"
+
+class UTileView;
+
+UCLASS()
+class THIRDMOTION_API ULibraryPanel : public UBaseWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	
+	
+	//UPROPERTY(meta=(BindWidgetOptional))
+	//UTileView* TileView;
+
+	
+};
