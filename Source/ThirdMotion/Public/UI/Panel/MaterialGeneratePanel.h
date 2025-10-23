5d8f983344c6c9f9a6a608b3c028586492c09c2e
Lauren <57754902+HyeseonLikesPizza@users.noreply.github.com>
2025-10-23 17:38:58 +0900

    [Temp] 임시 커밋

--- Source/ThirdMotion/Public/UI/Panel/MaterialGeneratePanel.h
+++ Source/ThirdMotion/Public/UI/Panel/MaterialGeneratePanel.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,9 +5,6 @@
 #include "UI/Widget/BaseWidget.h"
 #include "MaterialGeneratePanel.generated.h"
 
-/**
- * 
- */
 
 UENUM(BlueprintType)
 enum class EMaterialType : uint8
