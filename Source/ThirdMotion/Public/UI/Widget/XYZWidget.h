24cfb628eb34b2daf571d3ef4c1270eeedc77892
Lauren <57754902+HyeseonLikesPizza@users.noreply.github.com>
2025-10-29 17:52:40 +0900

    [Feat] XYZ 패널 구현 중

---
+++ Source/ThirdMotion/Public/UI/Widget/XYZWidget.h
@@ -0,0 +1,15 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseWidget.h"
+#include "XYZWidget.generated.h"
+
+
+UCLASS()
+class THIRDMOTION_API UXYZWidget : public UBaseWidget
+{
+	GENERATED_BODY()
+
+	
+};
